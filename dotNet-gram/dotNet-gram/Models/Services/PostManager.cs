9a4c2eda0d4bb6e6f15c97aa5ea2b2fa6324b1b5
Deziree' Teague <dez.teague@gmail.com>
2019-02-12 20:28:47 -0800

    added post manager service

---
+++ dotNet-gram/dotNet-gram/Models/Services/PostManager.cs
@@ -0,0 +1,31 @@
+﻿using dotNet_gram.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNet_gram.Models.Services
+{
+    public class PostManager : IPost
+    {
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> FindPost(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Post>> GetPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync(Post post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

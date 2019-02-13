1aebf0b77814e77e77aec3476748320524ed66b9
Deziree' Teague <dez.teague@gmail.com>
2019-02-12 22:49:57 -0800

    manage page works manually

--- dotNet-gram/dotNet-gram/Pages/Posts/Manage.cshtml.cs
+++ dotNet-gram/dotNet-gram/Pages/Posts/Manage.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotNet_gram.Models;
+using dotNet_gram.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,21 @@ namespace dotNet_gram.Pages.Posts
 {
     public class ManageModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IPost _post;
 
+        [FromRoute]
+        public int ID { get; set; }
+
+        public Post Post { get; set; }
+
+        public ManageModel(IPost post)
+        {
+            _post = post;
+        }
+
+        public async Task OnGet()
+        {
+            Post = await _post.FindPost(ID);
         }
     }
 }
